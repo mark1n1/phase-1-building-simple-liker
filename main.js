de2e2b1f649ba70093f44a2932b92f9243e44a5b
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-05-16 11:43:51 -0700

    Completed assignment

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeButtons = document.querySelectorAll('.like-glyph');
+console.log(likeButtons);
 
+likeButtons.forEach(function (likeButton) {
+  likeButton.addEventListener('click', handleLikeButton);
+});
 
-
+function handleLikeButton(event) {
+  mimicServerCall()
+  .then(() => {
+    const heart = event.target;
+    if(heart.innerHTML === EMPTY_HEART) {
+      heart.innerHTML = FULL_HEART;
+      heart.className = 'like-glyph activated-heart';
+    } 
+    else {
+      heart.innerHTML = EMPTY_HEART;
+      heart.className = 'like-glyph';
+    }
+  }).catch((error) => {
+    const div = document.getElementById('modal');
+    div.className = "";
+    document.getElementById('modal-message').innerHTML = error;
+    setTimeout(() => div.className = "hidden", 3000);
+  });
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
